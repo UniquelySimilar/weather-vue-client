0a20360d78a77fd10f4cf1c1a51e4101a1557ec0
UniquelySimilar <consciousexplorer@gmail.com>
2020-03-04 10:07:48 -0700

    retrieve data and add map markers

--- src/components/LocationWeatherMap.vue
+++ src/components/LocationWeatherMap.vue
@@ -8,26 +8,26 @@
                     {{ option.text }}
                 </option>
             </select>
-            <button type="button" id="retrieve-btn" v-on:click="initMap">Retrieve/Refresh Map Data</button>
+            <button type="button" id="retrieve-btn" v-on:click="retrieveDataRenderMap">Retrieve/Refresh Map Data</button>
         </header>
         <div id="map"></div>
     </div>
 </template>
 
 <script>
-    import { getOptions } from '../utils.js';
-    import 'ol/ol.css';
     import {Map, View} from 'ol';
     import TileLayer from 'ol/layer/Tile';
     import OSM from 'ol/source/OSM';
     import {fromLonLat} from 'ol/proj';
-    // import VectorLayer from 'ol/layer/Vector';
-    // import VectorSource from 'ol/source/Vector';
-    // import Feature from 'ol/Feature';
-    // import Point from 'ol/geom/Point';
-    // import {Icon, Style} from 'ol/style';
-    // import axios from 'axios';
-    // import IconImg from '../assets/marker.png';
+    import VectorLayer from 'ol/layer/Vector';
+    import VectorSource from 'ol/source/Vector';
+    import Feature from 'ol/Feature';
+    import Point from 'ol/geom/Point';
+    import {Icon, Style} from 'ol/style';
+    import IconImg from '../assets/marker.png';
+    import 'ol/ol.css';
+    import axios from 'axios';
+    import { getOptions } from '../utils.js';
 
     export default {
         name: "LocationWeatherMap",
@@ -35,28 +35,81 @@
             return {
                 numLocations: 10,
                 options: [],
-                map: undefined
+                map: null,
+                markerLayer: null,
+                iconStyle: null
             }
         },
         methods: {
-            initMap() {
-                console.log("called 'initMap()'");
-                this.map = new Map({
-                    target: 'map',
-                    layers: [
-                        new TileLayer({
-                            source: new OSM()
+            retrieveDataRenderMap() {
+                var that = this;
+                axios.get("http://localhost:8080/locationweatherapi/data", {
+                    params: {
+                        numLocations: this.numLocations
+                    }
+                })
+                .then(function (response) {
+                    //console.log(response.data);
+                    that.initMap(response.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+            },
+            initMap(locationWeatherData) {
+                console.log(locationWeatherData);
+                if (this.map === null) {
+                    this.map = new Map({
+                        target: 'map',
+                        layers: [
+                            new TileLayer({
+                                source: new OSM()
+                            })
+                        ],
+                        view: new View({
+                            center: fromLonLat([0, 0]),
+                            zoom: 0 // max: 28
                         })
-                    ],
-                    view: new View({
-                        center: fromLonLat([0, 0]),
-                        zoom: 0 // max: 28
+                    });
+                }
+
+                if (this.markerLayer !== null) {
+                    this.map.removeLayer(this.markerLayer);
+                }
+
+                // Add location markers
+                let featuresAry = [];
+                locationWeatherData.forEach(element => {
+                    let pointAry = [element.coord.lon, element.coord.lat];
+                    let feature = new Feature({
+                        geometry: new Point(fromLonLat(pointAry)),
+                        lon: element.coord.lon,
+                        lat: element.coord.lat,
+                        weatherDescription: element.weather[0] !== undefined ? element.weather[0].description : '',
+                        temp: element.main.temp,
+                        humidity: element.main.humidity,
+                        windSpeed: element.wind.speed
+                    });
+                    feature.setStyle(this.iconStyle);
+                    featuresAry.push(feature);
+                });
+
+                this.markerLayer = new VectorLayer({
+                    source: new VectorSource({
+                        features: featuresAry
                     })
                 });
-            }
+                this.map.addLayer(this.markerLayer);
+            },
         },
         created() {
             this.options = getOptions();
+            this.iconStyle = new Style({
+                image: new Icon({
+                    src: IconImg,
+                    scale: 0.05
+                })
+            });
         },
     }
 </script>
